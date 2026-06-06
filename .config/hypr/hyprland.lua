f1ad83533ecc5316c65e7620d9868c7bf4bae829
Krishna Raj B <bkr.2007@yahoo.com>
2026-06-06 15:49:39 +0530

    Add initial configuration for Hyprland

---
+++ .config/hypr/hyprland.lua
@@ -0,0 +1,18 @@
+---------------------
+---- MY PROGRAMS ----
+---------------------
+
+-- Set programs that you use
+terminal    = "kitty"
+fileManager = "nautilus"
+menu        = "~/.config/rofi/scripts/launcher.sh"
+
+require("modules.monitors")
+require("modules.binds")
+require("modules.autostart")
+require("modules.env")
+require("modules.decorations")
+require("modules.layout")
+require("modules.misc")
+require("modules.input")
+require("modules.windowrules")
